999dbfee836fb10ea922192530a1f8ca1fb7ffad
Usama Qazi <usamyharis@yahoo.com>
2023-07-25 22:05:02 +0500

    Task32, 33, 34, 35 done

--- A01/Task32/index.ts
+++ A01/Task32/index.ts
@@ -1,5 +1,18 @@
-let current_users: string[] = ["admin", "usama", "ali", "hamza", "haris"];
+let current_users: string[] = ["admin", "usama", "Ali", "hamza", "haris"];
 
-let new_users: string[] = ["usman", "shahzaib", "ali", "talha", "haris"];
+let new_users: string[] = ["usman", "ALI", "shahzaib", "haris", "talha"];
 
-let common
+function check_new_users(user: string[]) {
+  for (let username of user) {
+    let username_lower = username.toLowerCase();
+    if (current_users.includes(username_lower)) {
+      console.log(
+        `Username ${username_lower} already been used, enter a new username`
+      );
+    } else {
+      console.log(`Username ${username_lower} is available`);
+    }
+  }
+}
+
+check_new_users(new_users);
