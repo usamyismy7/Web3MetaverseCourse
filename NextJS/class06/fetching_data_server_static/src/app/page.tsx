c9ca9b52b576ed53a57e21652ed3380b5d8d64b2
Usama Qazi <usamyharis@yahoo.com>
2023-12-16 11:52:40 +0500

    Update changes

---
+++ NextJS/class06/fetching_data_server_static/src/app/page.tsx
@@ -0,0 +1,14 @@
+const getData = async () => {
+  const res = await fetch("https://api.quotable.io/random?tags=technology", {
+    cache: "no-store",
+  });
+  return res.json();
+};
+
+export default async function Home() {
+  const quote = await getData();
+
+  console.log("quote: ", quote);
+
+  return <div>{quote.content}</div>;
+}
