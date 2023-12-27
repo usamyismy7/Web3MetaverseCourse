953fe4882bf48bbad1a399cfd6320f719d75311d
Usama Qazi <usamyharis@yahoo.com>
2023-12-28 01:04:01 +0500

    update class 06 -> step 09 -> contentful code

--- NextJS/class06/step09/contentful/next.config.js
+++ NextJS/class06/step09/contentful/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["images.pexels.com", "images.ctfassets.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- NextJS/class06/step09/contentful/src/app/page.tsx
+++ NextJS/class06/step09/contentful/src/app/page.tsx
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-
-const inter = Inter({ subsets: ["latin"] });
+import Image from "next/image";
 
 async function getBlogs() {
   const res = await fetch(
@@ -10,26 +8,88 @@ async function getBlogs() {
       cache: "no-cache",
     }
   );
-
   if (!res.ok) throw new Error("Failed to fetch data");
-
   return res.json();
 }
-
 export default async function App() {
   const blogs = await getBlogs();
-  const items = blogs.items || []; // check if 'items' exists in the response
+  console.log(blogs);
   return (
     <div className="max-w-2xl m-6 mx-auto">
-      <ul>
-        {items.map((item: any) => (
-          <li key={item.sys.id}>
-            <h2>{item.fields.title}</h2>
-            {/* <p>{item.fields.description}</p> */}
-            {documentToReactComponents(item.fields.description)}
-          </li>
+      <h1 className="text-3xl font-bold">Blog</h1>
+      <div>
+        {blogs.items.map((item: any) => (
+          <>
+            <div>{item.fields.name}</div>
+            <div>{documentToReactComponents(item.fields.description)}</div>
+            <div>
+              {blogs.includes.Asset.map((asset: any) => (
+                <div key={item.fields.image.sys.id}>
+                  {item.fields.image.sys.id == asset.sys.id ? (
+                    <Image
+                      src={"https:" + asset.fields.file.url}
+                      alt=""
+                      width="400"
+                      height="400"
+                    />
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div>{documentToReactComponents(item.fields.description2)}</div>
+            <div>
+              {blogs.includes.Asset.map((asset: any) => (
+                <div key={item.fields.image2[0].sys.id}>
+                  {item.fields.image2.map((img2: any) => (
+                    <div key={img2.sys.id}>
+                      {img2.sys.id == asset.sys.id ? (
+                        <Image
+                          src={"https:" + asset.fields.file.url}
+                          alt=""
+                          width="400"
+                          height="400"
+                        />
+                      ) : (
+                        <div></div>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </>
         ))}
-      </ul>
+      </div>
     </div>
   );
+}
+
+{
+  /* when a single image is returned */
+}
+{
+  /* <Image src={data} alt="Trees" width={500} height={500} /> */
+}
+
+{
+  /* when an array of images is returned */
+}
+{
+  /* {data.sys.map(async (item: any, index: any) => {
+          let image: any = await getImage(item.id);
+          console.log(image);
+          return (
+            <div key={index}>
+              <Image src={image} alt="Trees" width={500} height={500} />
+            </div>
+          );
+        })} */
+}
+{
+  /* let image2Path: any = item.fields.image2;
+          let image2: any = await getImage(image2Path.sys.id);
+          console.log(image2);
+              <Image src={image2} alt="Foods" width={500} height={500} /> */
 }
